b291c6a4d2eafca99d313d85133727e145e6a628
Keyran <keyran@git.av>
2015-01-14 13:58:33 +0300

    Changed dependency from googleapiclient to google-api-python-client

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ import os
 from setuptools import setup, find_packages
 
 requires = ['python-dateutil', 'keyring', 'jinja2',
-            'markdown', 'PyYAML', 'pyinotify', 'googleapiclient',
+            'markdown', 'PyYAML', 'pyinotify', 'google-api-python-client',
             'transliterate']
 
 
